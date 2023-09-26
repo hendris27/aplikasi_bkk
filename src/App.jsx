6769d9e8740f8d802a5315ce87664eff3d1da14d
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-09-26 20:53:53 +0700

    add route page about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import About from './pages/About'
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/" element={<Profile />} />
+				<Route path="/" element={<About />} />
 			</Routes>
 		</BrowserRouter>
 	)
