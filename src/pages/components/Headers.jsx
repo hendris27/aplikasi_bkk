e3698e080a669c13814d5c05ec821ea41989ba04
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-09-29 21:52:15 +0700

    feat(headers components)

--- src/pages/components/Headers.jsx
+++ src/pages/components/Headers.jsx
@@ -1,4 +1,13 @@
 const Headers = () => {
-	return <>Headers</>
+	return (
+		<>
+			<div className="bg-accent flex justify-center gap-8 py-4 font-bold ">
+				<div className="hover:font-extrabold cursor-pointer">Home</div>
+				<div className="hover:font-extrabold cursor-pointer">About</div>
+				<div className="hover:font-extrabold cursor-pointer">Portopolio</div>
+				<div className="hover:font-extrabold cursor-pointer">Contact</div>
+			</div>
+		</>
+	)
 }
 export default Headers
