a1aa405fe5743898e914e612e3554db9aac5bee1
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-10-01 23:15:00 +0700

    feat(add headers components)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,12 @@
+import Headers from './components/Headers'
+
 const About = () => {
-	return <>About</>
+	return (
+		<>
+			<div className="bg-accent">
+				<Headers />
+			</div>
+		</>
+	)
 }
 export default About
