062dd19465e2c3d0c87f3d0c257366442e71d6a4
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-10-08 21:41:32 +0700

    add page contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import Headers from './components/Headers'
+
+const contact = () => {
+	return (
+		<>
+			<div className="bg-accent">
+				<Headers />
+			</div>
+			<div>ini halaman contact</div>
+		</>
+	)
+}
+export default contact
