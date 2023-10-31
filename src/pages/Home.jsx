f408902e07a197142716b716bd80f6ddb7a6394e
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-10-31 22:18:10 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,17 @@ const Home = () => {
 			<div className="bg-accent">
 				<Headers />
 			</div>
-			<div>ini halaman home</div>
+			<div className="h-screen">
+				<div className="flex justify-between h-full">
+					<div className="bg-red-300 flex justify-center items-center w-[50%]">
+						<div className="bg-green-100 text-bold text-[40px] pl-8">
+							<div>Hello</div>
+							<div>I am Hendri</div>
+						</div>
+					</div>
+					<div className="bg-red-100 flex-1">1</div>
+				</div>
+			</div>
 		</>
 	)
 }
