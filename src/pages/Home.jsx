3c3a3b85efb2b0d6c716eadebda8fdcd43ed222b
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-11-03 21:39:42 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Headers from './components/Headers'
+import icon_hello from './assets/img/icon_hello.png'
 
 const Home = () => {
 	return (
@@ -9,9 +10,19 @@ const Home = () => {
 			<div className="h-screen">
 				<div className="flex justify-between h-full">
 					<div className="bg-red-300 flex justify-center items-center w-[50%]">
-						<div className="bg-green-100 text-bold text-[40px] pl-8">
-							<div>Hello</div>
-							<div>I am Hendri</div>
+						<div className="bg-green-100 pl-8 flex flex-col gap-[-80px] ">
+							<div className="flex gap-4 items-center">
+								<div className="text-blue-600 font-bold text-[50px]">Hello</div>
+								<div className="h-full">
+									<img className="h-12 " src={icon_hello} />
+								</div>
+								<div className="text-blue-600 text-[50px] text-bold">,</div>
+								<div className="text-[60px] ">I&#39;m Hendri</div>
+							</div>
+							<div className="text-[60px] flex gap-4">
+								<div className="text-[] ">I&#39;m an</div>
+								<div className="text-bold text-blue-600 font-semibold ">Web Developer</div>
+							</div>
 						</div>
 					</div>
 					<div className="bg-red-100 flex-1">1</div>
